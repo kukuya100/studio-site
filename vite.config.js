c40918116714acfc790c6e8a6694178b58c2cc88
kukuya100 <95200162+ForestChad@users.noreply.github.com>
2025-09-15 13:41:20 +0900

    vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // ← 레포 이름으로 바꾸세요 (예: /my-portfolio/)
-  base: '/studio-site/',
+  base: '/',   // 커스텀 도메인 사용 시 루트 경로
 })
